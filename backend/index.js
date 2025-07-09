a7f26addbb553900d2da3956433c5f79aa64584d
Pabitra Ghara <pabitraghara384@gmail.com>
2025-07-09 22:56:08 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ const port = 5001;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send({ activeStatus: true, error: false });
+});
+
 // Available Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
