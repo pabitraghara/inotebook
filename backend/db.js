581971e808a38c9dda1b895e2f7b92451782353f
Pabitra Ghara <pabitraghara384@gmail.com>
2025-07-10 12:56:40 +0530

    change mongo uri

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,10 @@
-
-const mongoose = require('mongoose');
-const mongoURI ="mongodb://0.0.0.0/inotebook"
+const mongoose = require("mongoose");
+const mongoURI = process.env.MONGO_URI;
 
 const connectToMongo = async () => {
-    mongoose.connect(mongoURI,()=>{
-        console.log("Connected to MongoDB successfully");
-    })
+  mongoose.connect(mongoURI, () => {
+    console.log("Connected to MongoDB successfully");
+  });
 };
 
 module.exports = connectToMongo;
-
